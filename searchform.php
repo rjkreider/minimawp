284add66bd66ba10d5a6d8f9cbc37802dd71a3bf
Rich Kreider <rjkreider@gmail.com>
2025-11-26 15:18:04 -0500

    removed personalizations, fixed input text colors

--- searchform.php
+++ searchform.php
@@ -5,6 +5,6 @@
 ?>
 <form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="search-wrap">
-        <input type="search" placeholder="<?php echo esc_attr( 'pick my brain...', 'presentation' ); ?>" name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" />
+        <input type="search" placeholder="<?php echo esc_attr( 'Search...', 'presentation' ); ?>" name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" />
     </div>
 </form>
